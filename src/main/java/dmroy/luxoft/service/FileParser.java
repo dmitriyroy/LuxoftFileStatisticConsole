27324447c9ef1fca96266fe9c6f1d937c9a675ec
dmitriyroy <dmitriyroy@dima>
2016-11-08 22:09:06 +0200

    Добавил поле FILE_ID

--- src/main/java/dmroy/luxoft/service/FileParser.java
+++ src/main/java/dmroy/luxoft/service/FileParser.java
@@ -3,21 +3,18 @@ package dmroy.luxoft.service;
 import dmroy.luxoft.Main;
 import dmroy.luxoft.been.Line;
 import dmroy.luxoft.util.FileUtils;
-import java.awt.FileDialog;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.SortedSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
 
 /**
  *
@@ -141,6 +138,7 @@ public class FileParser implements FileParserInterface{
                     averageWordLength = (int)wordsLength/wordSet.size();
                     
                     Line line = new Line();
+                    line.setFileId(new Date().getTime());
                     line.setFileName(file.getAbsolutePath());
                     line.setLineNumber(lineNumber);
                     line.setMinWord(minWord);
