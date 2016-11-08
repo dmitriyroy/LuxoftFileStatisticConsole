accd4caebe527a1ec52e641152482c880b8e5e2d
dmitriyroy <dmitriyroy@dima>
2016-11-08 23:49:10 +0200

    чистка ненужного кода

--- src/main/java/dmroy/luxoft/service/FileParserInterface.java
+++ src/main/java/dmroy/luxoft/service/FileParserInterface.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dmroy.luxoft.service;
 
 import dmroy.luxoft.been.Line;
-import java.io.File;
 import java.util.List;
 
 /**
@@ -14,7 +8,5 @@ import java.util.List;
  * @author dmitriyroy
  */
 public interface FileParserInterface {
-//    public File getFileToParse();
-//    public File getFileForLog();
     public List<Line> parseFile();
 }
