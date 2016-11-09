f91474e59cbe51246fd0c7ddaaa38a8fa25aa362
Дмитрий Рой < @dmitriyroy>
2016-11-09 18:09:19 +0200

    чистка

--- src/main/java/dmroy/luxoft/util/FileUtils.java
+++ src/main/java/dmroy/luxoft/util/FileUtils.java
@@ -2,6 +2,7 @@ package dmroy.luxoft.util;
 
 import java.awt.FileDialog;
 import java.io.File;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
@@ -10,20 +11,22 @@ import javax.swing.WindowConstants;
  * @author dmitriyroy
  */
 public class FileUtils {
-    
+
     public static File getFile(String dialogName){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setAlwaysOnTop(true);
         frame.setEnabled(true);
-        
+
         FileDialog dialog = new FileDialog(frame,dialogName);
-//        dialog.setFocusable(true);
+        dialog.setFocusable(true);
         dialog.setAlwaysOnTop(true);
         dialog.setVisible(true);
         String filePath = dialog.getDirectory() + dialog.getFile();
         frame.dispose();
 //        System.out.println("filePath = " + filePath);
+//        JFileChooser dialog1 = new JFileChooser("Вася");
+//        return new File(dialog.getCurrentDirectory().getAbsolutePath());
         return new File(filePath);
     }
 }
